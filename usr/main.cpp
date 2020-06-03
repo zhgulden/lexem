183be28514510ed9f75af916961c82b2a43d09f4
Gulden Maratovna <55193105+zhgulden@users.noreply.github.com>
2020-06-03 06:06:48 +0600

    Delete comments

--- usr/main.cpp
+++ usr/main.cpp
@@ -9,11 +9,7 @@ int main(void) {
 		std::cout << "> ";
 		getline(std::cin, codeline);
 		infix = parse_lexem(codeline);
-		// std::cout << "INFIX:" << std::endl;
-		//print_vector(infix);
 		postfix = build_postfix(infix);
-		//std::cout << "POSTFIX:" << std::endl;
-		//print_vector(postfix);
 		int value = evaluate_postfix(postfix);
 		std::cout << ">>>> " << value << std::endl;
 	} while (codeline != "quit");
